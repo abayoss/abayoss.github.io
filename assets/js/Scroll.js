50508470183e264aab048d69463f9d4247bed849
abayoss <abayoss@yahoo.com>
2019-04-21 17:06:36 +0100

    scroll

---
+++ assets/js/Scroll.js
@@ -0,0 +1,10 @@
+const slideRight = {
+    distance: '2%',
+    origin: 'left',
+    opacity: 0,
+    delay: 200,
+    // reset: true,
+    interval: 16
+};
+
+ScrollReveal().reveal('.slideRight', slideRight)
