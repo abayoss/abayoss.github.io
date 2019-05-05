f3d722bc45d54970559fb89f652a8ca2ce14d736
abayoss <abayoss@yahoo.com>
2019-05-05 16:28:34 +0000

    image bg and small modification

--- assets/js/AnimationOnScroll.js
+++ assets/js/AnimationOnScroll.js
@@ -1,28 +1,23 @@
 const slideRight = {
-    distance: '6%',
-    origin: 'left',
-    opacity: 0,
-    delay: 600,
-    // reset: true,
-    interval: 400
+  origin: "left",
+  delay: 600,
+  // reset: true,
+  interval: 100
 };
 const slideLeft = {
-    distance: '6%',
-    origin: 'right',
-    opacity: 0,
-    delay: 800,
-    // reset: true,
-    interval: 600
+  origin: "right",
+  // delay: 600,
+  // reset: true,
+  interval: 600
 };
 const slideUp = {
-    distance: '100%',
-    origin: 'bottom',
-    opacity: 0,
-    delay: 800,
-    // reset: true,
-    interval: 600
+  distance: "100%",
+  origin: "bottom",
+  // delay: 600,
+  // reset: true,
+  interval: 600
 };
-
-ScrollReveal().reveal('.slideRight', slideRight);
-ScrollReveal().reveal('.slideLeft', slideLeft);
-ScrollReveal().reveal('.slideUp', slideUp);
+ScrollReveal({ mobile: false, opacity: 0, distance: "6%" });
+ScrollReveal().reveal(".slideRight", slideRight);
+ScrollReveal().reveal(".slideLeft", slideLeft);
+ScrollReveal().reveal(".slideUp", slideUp);
