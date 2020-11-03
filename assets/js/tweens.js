8632a2bea2344c3267cd68f0019348d101924a11
abayoss <abaayous.oussama@gmail.com>
2020-11-03 13:46:54 +0100

    markers off

--- assets/js/tweens.js
+++ assets/js/tweens.js
@@ -1,12 +1,12 @@
 const tl = gsap.timeline({
     scrollTrigger: {
         trigger: '#work',
-        start: 'top bottom',
+        start: 'top 94%',
         end: 'bottom top',
         scrub: true,
-        markers: true,
+        // markers: true,
     },
 });
-tl.to('.bg-black-img', { scale: 1.5 , y: 100});
+tl.to('.bg-black-img', { scale: 1.5 , y: 500});
 // tl.to('.box', { rotation: 360, duration: 3 });
 // tl.to('.box', { xPercent: 350, duration: 1 });
