121829e7b2612b6770c876ea0e33b6360d6df121
abayoss <abaayous.oussama@gmail.com>
2020-11-05 17:18:19 +0100

    trying imag paralax tween

--- assets/js/tweens.js
+++ assets/js/tweens.js
@@ -1,23 +1,25 @@
-// const tl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.bg-black-img',
-//         start: 'bottom 90%',
-//         end: 'bottom top',
-//         scrub: true,
-//         // markers: true,
-//     },
-// });
-// tl.to('.bg-black-img', { scale: 1.2, y: 100 })
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.bg-black-img',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+        markers: true,
+        id:"black_img"
+    },
+});
+tl.to('.bg-black-img', { scale: 1.1, y: 100, x:-100 })
 
 const tl2 = gsap.timeline({
     scrollTrigger: {
-        trigger: '#section-paralax',
-        start: 'top bottom',
-        end: 'bottom 20%',
+        trigger: '#img-paralax-col',
+        start: '30% bottom',
+        end: '130% top',
         scrub: true,
         markers: true,
+        id:"project_img"
     },
 });
-tl2.to('#img-paralax', {y: 100 })
+tl2.to('#img-paralax', { y: 60 })
 
-SmoothScroll({ animationTime : 900, stepSize: 60 })
+SmoothScroll({ animationTime : 1000, stepSize: 70 });
