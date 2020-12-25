5e1b7d3304b80ab6a918e7ce317434a888c628d5
abayoss <abaayous.oussama@gmail.com>
2020-12-25 17:40:03 +0100

    pwa testing

--- src/app.js
+++ src/app.js
@@ -4,12 +4,16 @@ import { animateOnscroll } from './js/pages/home/AnimationOnScroll';
 // import './js/ScrollButton';
 import { loadGsapIntroScrollTween } from './js/pages/home/tweens';
 
-console.log(projects);
-
 var ready = (callback) => {
     if (document.readyState != 'loading') callback();
     else document.addEventListener('DOMContentLoaded', callback);
 };
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('./sw.js')
+//         .then(reg => console.log('service worker registered', reg))
+//         .catch(err => console.log('service worker not registered', err));
+// }
 
 ready(() => {
     SmoothScroll({ stepSize: 60, animationTime: 1500 });
@@ -27,11 +31,11 @@ ready(() => {
                 // fetch('assets/data/' + env + '/projects.json?' + Math.random())
                 //     .then((res) => res.json())
                 //     .then((responseData) => {
-                        // this.ProjectsList = responseData.projects.slice(0, 6);
-                        this.ProjectsList = projects.projects.slice(0, 6);
-                        animateOnscroll();
-                        loadGsapIntroScrollTween();
-                    // });
+                // this.ProjectsList = responseData.projects.slice(0, 6);
+                this.ProjectsList = projects.projects.slice(0, 6);
+                animateOnscroll();
+                loadGsapIntroScrollTween();
+                // });
             },
         },
     });
---
+++ src/sw.js
@@ -0,0 +1,56 @@
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'dynamic-cache-v1';
+const assets = [
+    '/',
+    '/index.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/css/uikit.min.css',
+    'https://bootswatch.com/4/cyborg/bootstrap.min.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://fonts.googleapis.com/css?family=Alata&display=swap',
+    'https://cdn.jsdelivr.net/npm/vue/dist/vue.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js',
+    'https://unpkg.com/scrollreveal/dist/scrollreveal.min.js',
+];
+
+// install event
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        }).catch(() => caches.match('/pages/fallback.html'))
+    );
+});
